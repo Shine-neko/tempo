167402186862246f1a50a56686f75a37e0f5820b
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-05-31 13:52:08 +0200

    Fix typo

---
+++ src/Tempo/Bundle/MainBundle/Repository/ChatMessageRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+* This file is part of the Tempo-project package http://tempo-project.org/>.
+*
+* (c) Mlanawo Mbechezi  <mlanawo.mbechezi@ikimea.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Tempo\Bundle\MainBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+
+Class ChatMessageRepository extends EntityRepository
+{
+    public function getMessages($room)
+    {
+        return
+            $this->createQueryBuilder('r')
+                ->where('r.room = :room')
+                ->setParameter('room', $room)
+            ;
+    }
+}
