46fbb23b314c267f2c8126dad411aa2976740d49
Mlanawo Mbechezi <mlanawo.mbechezi@ikimea.com>
2015-02-27 13:23:03 +0100

    Configure SyliusConfiguration

---
+++ src/Tempo/Bundle/AppBundle/Model/Parameter.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+* This file is part of the Tempo-project package http://tempo-project.org/>.
+*
+* (c) Mlanawo Mbechezi  <mlanawo.mbechezi@ikimea.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Tempo\Bundle\AppBundle\Model;
+
+use Sylius\Bundle\SettingsBundle\Model\Parameter as BaseParameter;
+
+class Parameter extends BaseParameter
+{
+}
