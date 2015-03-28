cda7567de2ae9518f8a968ff0356a68c0efc2b34
Mlanawo Mbechezi <mlanawo.mbechezi@ikimea.com>
2015-03-28 14:22:51 +0100

    add provider deploy

---
+++ src/Tempo/Bundle/AppBundle/Provider/DeployProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+* This file is part of the Tempo-project package http://tempo-project.org/>.
+*
+* (c) Mlanawo Mbechezi  <mlanawo.mbechezi@ikimea.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Tempo\Bundle\AppBundle\Provider;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class DeployProvider implements ProviderInterface
+{
+    public function parse(Request $request)
+    {
+        $request->headers->get('X-provider');
+        $payload = $request->request->get('data');
+
+        $activity = new ActivityProvider();
+        $activity->setMessage('Branch master (at '.$payload['release']['sha'].') deployed as release '.$payload['release']['name'].' by '.$payload['author']['name']);
+        $activity->setCreatedAt(new \DateTime());
+        $activity->setParameters($payload);
+
+        return $activity;
+    }
+
+    public function getName()
+    {
+        return 'Deploy';
+    }
+
+    public function getCanonicalName()
+    {
+        return 'deploy';
+    }
+}
