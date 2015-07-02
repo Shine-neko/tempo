1b72e707a847e4c006bcfd5337b3350fb2eb7d3d
Mlanawo Mbechezi <mlanawo.mbechezi@ikimea.com>
2015-07-02 03:06:08 +0200

    Introduce Mailer

---
+++ src/Tempo/Bundle/AppBundle/Event/MailerEvent.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+* This file is part of the Tempo-project package http://tempo-project.org/>.
+*
+* (c) Mlanawo Mbechezi  <mlanawo.mbechezi@ikimea.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Tempo\Bundle\AppBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class MailerEvent extends Event
+{
+    /**
+     * @var string
+     */
+    protected $subject;
+
+    /**
+     * @var string
+     */
+    protected $content;
+
+    public function __construct($subject, $content)
+    {
+        $this->subject = $subject;
+        $this->content = $content;
+    }
+}
