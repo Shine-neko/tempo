acd188f14e5e2167acf53b74a44a9e68aa97ccb9
Mlanawo Mbechezi <mlanawo.mbechezi@ikimea.com>
2015-09-11 17:13:38 +0200

    Improve ClassUtils

--- src/Tempo/Bundle/AppBundle/Util/ClassUtils.php
+++ src/Tempo/Bundle/AppBundle/Util/ClassUtils.php
@@ -15,10 +15,48 @@ use Doctrine\Common\Util\ClassUtils as BaseClassUtils;
 
 class ClassUtils
 {
-    public static function getShortName($class)
+    /**
+     * @param $class
+     * @return string
+     */
+    public static function getShortName($class, $capitalized = true)
     {
-        $resourceName = BaseClassUtils::newReflectionClass($class);
+        if (is_object($class)) {
+            $class = BaseClassUtils::getClass($class);
+        }
 
-        return strtolower($resourceName->getShortName());
+        $resourceName = BaseClassUtils::newReflectionClass($class)->getShortName();
+
+        if ($capitalized) {
+            $resourceName = strtolower($resourceName);
+        }
+
+        return $resourceName;
+    }
+
+    /**
+     * Reverse a CamelCase string.
+     *
+     * Examples:
+     *     uncamel('lowerCamelCase') === 'lower_camel_case'
+     *     uncamel('UpperCamelCase') === 'upper_camel_case'
+     *     uncamel('ThisIsAString') === 'this_is_a_string'
+     *     uncamel('notcamelcase') === 'notcamelcase'
+     *     uncamel('lowerCamelCase', ' | ') === 'lower | camel | case'
+     *
+     * @link      http://stackoverflow.com/a/1993772/5103614
+     *
+     * @param  string $content The CamelCase string.
+     * @param  string $separator The glue for the compound words. Defaults to '_'.
+     * @return string
+     */
+    public static function uncamel($content, $separator = '_')
+    {
+        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $content, $matches);
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+        return implode($separator, $ret);
     }
 }
